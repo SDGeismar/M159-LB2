c5a2a77af9c3a8b4645bfbfff5716468a92812d5
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2017-11-23 22:42:35 +0100

    Update step3-config_services.ps1

--- scripts/step3-config_services.ps1
+++ scripts/step3-config_services.ps1
@@ -1,7 +1,9 @@
 # Post ADDS-deployment configuration
 # This script configures DHCP, OUs, DNS and all that crap
 
-$cn = hostname
+Import-Module ActiveDirectory 
+
+$dc = hostname
 $ip = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
 
 # DHCP config
@@ -10,12 +12,10 @@ echo "Now installing and authorizing DHCP Service"
 Install-WindowsFeature DHCP -IncludeManagementTools
 Add-DhcpServerInDC -DnsName ADS.M159.iet-gibb.ch -IPAddress $ip
 echo " "
-echo "Now setting Scope on CN=ADS"
-Add-DhcpServerv4Scope -Name 'ADS DHCP' -StartRange 192.168.210.1 -EndRange 192.168.210.100 -SubnetMask 255.255.255.0 -Description 'ADS DHCP default scope' –cn $cn
+echo "Now configuring scope on ADS.local"
+Add-DhcpServerv4Scope -Name 'ADS DHCP' -StartRange 192.168.210.1 -EndRange 192.168.210.100 -SubnetMask 255.255.255.0 -Description 'ADS DHCP default scope' –cn $dc
 echo " "
 
 # OUs
 echo "Now setting up OUs"
-New-ADOrganizationalUnit -Name "Management"
-New-ADOrganizationalUnit -Name "EDV"
-New-ADOrganizationalUnit -Name "Produktion"
+import-csv ou.csv -delimiter ";"| New-ADOrganizationalUnit –PassThru
