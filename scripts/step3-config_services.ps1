7a5db6960c426dca0c59b56038f8292cad78cd64
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2017-11-23 23:00:35 +0100

    Update step3-config_services.ps1

--- scripts/step3-config_services.ps1
+++ scripts/step3-config_services.ps1
@@ -12,10 +12,12 @@ echo "Now installing and authorizing DHCP Service"
 Install-WindowsFeature DHCP -IncludeManagementTools
 Add-DhcpServerInDC -DnsName ADS.M159.iet-gibb.ch -IPAddress $ip
 echo " "
-echo "Now configuring scope on ADS.local"
+echo "Now setting Scope on CN=ADS"
 Add-DhcpServerv4Scope -Name 'ADS DHCP' -StartRange 192.168.210.1 -EndRange 192.168.210.100 -SubnetMask 255.255.255.0 -Description 'ADS DHCP default scope' –cn $dc
 echo " "
 
 # OUs
 echo "Now setting up OUs"
 import-csv ou.csv -delimiter ";"| New-ADOrganizationalUnit –PassThru
+echo "Now setting up Users"
+Import-Csv users.csv -delimiter ";" | New-ADUser -PassThru | Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText ‘asdf1234’ -Force) -PassThru | Enable-ADAccount
