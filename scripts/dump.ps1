c1e7b739092907c6994a8afad1963f00cdefd8b2
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2017-11-25 13:56:29 +0100

    Update dump.ps1

--- scripts/dump.ps1
+++ scripts/dump.ps1
@@ -2,3 +2,5 @@
 # The script itself serves no purpse
 
 Get-ADObject -SearchBase (Get-ADRootDSE).ConfigurationNamingContext -filter "objectclass -eq 'site'" | Rename-ADObject -NewName Thun
+
+Get-ADForest ADS.M159.iet-gibb.ch | FL GlobalCatalogs
